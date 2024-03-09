34559c27ea72bcfbb9028389be355b37afd9d147
John Dilworth <161086942+jdil7@users.noreply.github.com>
2024-03-08 22:47:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Dilworth World!'));
+git add app.js
+git commit -m "Full pipeline test"
+git push
+app.get('/', (req, res) => res.send('John Dilworth World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
